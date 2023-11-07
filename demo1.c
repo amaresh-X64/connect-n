ff06bc57d6a192cc6ae6061c21cb1232fc70393d
amaresh-X64 <144317198+amaresh-X64@users.noreply.github.com>
2023-11-07 12:25:50 +0530

    Update demo1.c

--- demo1.c
+++ demo1.c
@@ -18,7 +18,12 @@ void printBoard(int ROWS,int COLUMNS,char (*board)[COLUMNS]) {
     }
     printf("  %d",1);
     for (int col = 2; col < COLUMNS+1; col++)
+    {
+    if(col<=10)
     printf("   %d",col);
+    if(col>10)
+    printf("  %d",col);
+    }
     printf("\n");
 }
 
@@ -29,11 +34,10 @@ int dropPiece(int col, char player,int ROWS,int COLUMNS,char (*board)[COLUMNS]) 
             return 1;
         }
     }
-    return 0; // Column is full
+    return 0;
 }
 
 int checkWin(char player,int ROWS,int COLUMNS,char (*board)[COLUMNS]) {
-    // Check for horizontal wins
     for (int row = 0; row < ROWS; row++) {
         for (int col = 0; col <= COLUMNS - 4; col++) {
             if (board[row][col] == player &&
@@ -45,7 +49,6 @@ int checkWin(char player,int ROWS,int COLUMNS,char (*board)[COLUMNS]) {
         }
     }
 
-    // Check for vertical wins
     for (int col = 0; col < COLUMNS; col++) {
         for (int row = 0; row <= ROWS - 4; row++) {
             if (board[row][col] == player &&
@@ -57,7 +60,6 @@ int checkWin(char player,int ROWS,int COLUMNS,char (*board)[COLUMNS]) {
         }
     }
 
-    // Check for diagonal wins (bottom-left to top-right)
     for (int row = 3; row < ROWS; row++) {
         for (int col = 0; col <= COLUMNS - 4; col++) {
             if (board[row][col] == player &&
@@ -68,9 +70,7 @@ int checkWin(char player,int ROWS,int COLUMNS,char (*board)[COLUMNS]) {
             }
         }
     }
-
-    // Check for diagonal wins (bottom-right to top-left)
-    for (int row = 3; row < ROWS; row++) {
+for (int row = 3; row < ROWS; row++) {
         for (int col = 3; col < COLUMNS; col++) {
             if (board[row][col] == player &&
                 board[row - 1][col - 1] == player &&
@@ -81,7 +81,6 @@ int checkWin(char player,int ROWS,int COLUMNS,char (*board)[COLUMNS]) {
         }
     }
 
-    // Check for crossward consecutive 1s
     for (int row = 0; row < ROWS - 3; row++) {
         for (int col = 0; col < COLUMNS - 3; col++) {
             if (board[row][col] == player &&
@@ -106,18 +105,44 @@ int checkWin(char player,int ROWS,int COLUMNS,char (*board)[COLUMNS]) {
 
     return 0;
 }
+char fun(int currentplayer)
+{
+        switch(currentplayer)
+        {
+            case 1:
+            return 'X';
+            case 2:
+            return 'O';
+            case 3:
+            return 'Y';
+            case 4:
+            return 'Z';
+            case 5:
+            return 'A';
+        }
+}
 
 int main() {
-    int ROWS,COLUMNS;
+    int ROWS,COLUMNS,players;
     printf("enter number of rows greater than 6\n");
     scanf("%d",&ROWS);
     printf("enter number ofcolumns greater than 6\n");
     scanf("%d",&COLUMNS);
+    printf("enter number of players less than 5\n");
+    scanf("%d",&players);
+    while(players>5)
+    {
+    printf("enter number of players less than 5\n");
+    scanf("%d",&players);
+    }
+
     char board[ROWS][COLUMNS];
     initializeBoard(ROWS,COLUMNS,board);
     int currentPlayer = 1;
     int moves = 0;
-
+    
+    if(players==2)
+    {
     while (1) {
         printf("Current board:\n");
         printBoard(ROWS,COLUMNS,board);
@@ -149,6 +174,153 @@ int main() {
             printf("Column is full. Try again.\n");
         }
     }
+    }
+    if(players==3)
+    {
+        while (1) {
+        printf("Current board:\n");
+        printBoard(ROWS,COLUMNS,board);
 
+        int col;
+        printf("Player %d, enter a column (1-7): ", currentPlayer);
+        scanf("%d", &col);
+        col--;
+
+        if (col < 0 || col >= COLUMNS) {
+            printf("Invalid column. Try again.\n");
+            continue;
+        }
+
+        if (dropPiece(col,fun(currentPlayer),ROWS,COLUMNS,board)) {
+            if (checkWin(fun(currentPlayer),ROWS,COLUMNS,board)) {
+                printBoard(ROWS,COLUMNS,board);
+                printf("Player %d wins!\n", currentPlayer);
+                break;
+            }
+            moves++;
+            if (moves == ROWS * COLUMNS) {
+                printf("It's a draw!\n");
+                printBoard(ROWS,COLUMNS,board);
+                break;
+            }
+            switch(currentPlayer)
+            {
+            case 1:
+                currentPlayer=2;
+                break;
+            case 2:
+                currentPlayer=3;
+                break;
+            case 3:
+                currentPlayer=1;
+                break;
+            }
+        } 
+        else {
+            printf("Column is full. Try again.\n");
+        }
+    }
+    }
+    if(players==4)
+    {
+        while (1) {
+        printf("Current board:\n");
+        printBoard(ROWS,COLUMNS,board);
+
+        int col;
+        printf("Player %d, enter a column (1-7): ", currentPlayer);
+        scanf("%d", &col);
+        col--;
+
+        if (col < 0 || col >= COLUMNS) {
+            printf("Invalid column. Try again.\n");
+            continue;
+        }
+
+        if (dropPiece(col,fun(currentPlayer),ROWS,COLUMNS,board)) {
+            if (checkWin(fun(currentPlayer),ROWS,COLUMNS,board)) {
+                printBoard(ROWS,COLUMNS,board);
+                printf("Player %d wins!\n", currentPlayer);
+                break;
+            }
+            moves++;
+            if (moves == ROWS * COLUMNS) {
+                printf("It's a draw!\n");
+                printBoard(ROWS,COLUMNS,board);
+                break;
+            }
+            switch(currentPlayer)
+            {
+            case 1:
+                currentPlayer=2;
+                break;
+            case 2:
+                currentPlayer=3;
+                break;
+            case 3:
+                currentPlayer=4;
+                break;
+            case 4:
+                currentPlayer=1;
+                break;
+            }
+        } 
+        else {
+            printf("Column is full. Try again.\n");
+        }
+    }
+    }
+    if(players==5)
+    {
+        while (1) {
+        printf("Current board:\n");
+        printBoard(ROWS,COLUMNS,board);
+
+        int col;
+        printf("Player %d, enter a column (1-7): ", currentPlayer);
+        scanf("%d", &col);
+        col--;
+
+        if (col < 0 || col >= COLUMNS) {
+            printf("Invalid column. Try again.\n");
+            continue;
+        }
+
+        if (dropPiece(col,fun(currentPlayer),ROWS,COLUMNS,board)) {
+            if (checkWin(fun(currentPlayer),ROWS,COLUMNS,board)) {
+                printBoard(ROWS,COLUMNS,board);
+                printf("Player %d wins!\n", currentPlayer);
+                break;
+            }
+            moves++;
+            if (moves == ROWS * COLUMNS) {
+                printf("It's a draw!\n");
+                printBoard(ROWS,COLUMNS,board);
+                break;
+            }
+            switch(currentPlayer)
+            {
+            case 1:
+                currentPlayer=2;
+                break;
+            case 2:
+                currentPlayer=3;
+                break;
+            case 3:
+                currentPlayer=4;
+                break;
+            case 4:
+                currentPlayer=5;
+                break;
+            case 5:
+                currentPlayer=1;
+                break;
+            }
+        } 
+        else {
+            printf("Column is full. Try again.\n");
+        }
+    }
+    }
     return 0;
 }
